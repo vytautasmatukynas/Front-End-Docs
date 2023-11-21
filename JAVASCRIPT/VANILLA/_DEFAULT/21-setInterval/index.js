b4611fe6f030d2bccc3e42eb1ed4da30f6e34174
Vytautas <51360361+vytautasmatukynas@users.noreply.github.com>
2023-11-21 10:02:19 +0200

    updated

--- JAVASCRIPT/VANILLA/_DEFAULT/21-setInterval/index.js
+++ JAVASCRIPT/VANILLA/_DEFAULT/21-setInterval/index.js
@@ -12,3 +12,29 @@ setTimeout(function() {
   clearInterval(intervalId);
   console.log("Interval stopped after 10 seconds.");
 }, 10000);
+
+
+function move() {
+  let element = document.getElementById("movingBox");
+  let position = 0;
+  let direction = 1; // 1 for moving right, -1 for moving left
+
+  // Set an interval to update the position every 10 milliseconds
+  let interval = setInterval(() => {
+    position += direction; // Adjust the speed by changing this value
+    element.style.left = position + "px";
+
+    // Change direction when the box reaches the edges
+    if (position >= window.innerWidth - element.offsetWidth || position <= 0) {
+      direction *= -1;
+    }
+
+    // Stop the movement when it reaches the starting position
+    if (position === 0) {
+      clearInterval(interval);
+    }
+  });
+}
+
+// Call the move function when the page loads
+window.onload = move;
